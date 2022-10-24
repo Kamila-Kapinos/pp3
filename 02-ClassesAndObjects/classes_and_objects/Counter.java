00a1547582495007aa2ed6eb770ff433107ad158
Kamila-Kapinos <s221572@student.uek.krakow.pl>
2022-10-24 17:53:27 +0200

    fixing

--- 02-ClassesAndObjects/classes_and_objects/Counter.java
+++ 02-ClassesAndObjects/classes_and_objects/Counter.java
@@ -1,38 +1,55 @@
-public class Counter{
+public class Counter{  
 
     //atrybuty
-    
-    private int counter = 0;
-    
+   
+    private int counter = Constans.INITIAL_VALUE_STATE;
+   
     //metody
-    
+   
     public int increase(){
-    
+   
         return this.counter++;
     }
-    
+
+   
     public int decrease(){
-    
+   
+        return this.counter--;
+    }
+
+    public int increase10(){
+   
+        return this.counter += 10;
+    }
+
+   
+    public int decrease10(){
+
+   
         return this.counter -= 10;
     }
-    
+
+   
     public void resetCounter(){
-    
+   
         this.counter = Constans.INITIAL_VALUE_STATE;
     }
-    
+   
     public void showValue(){
-    
+   
         System.out.println("Counter value is: " + this.counter);
     }
-
-
+    
     public static void main(String[] args){
         Counter c1 = new Counter();
         
-        for (int i = 0; i < 23; i++) {
-              c1.increase();
+        for (int i = 0; i < 2; i++) {
+              c1.increase10();
             }
+        
+        for(int i = 0; i < 3; i++){
+            c1.increase();
+        }
             
         c1.showValue();
         c1.resetCounter();
